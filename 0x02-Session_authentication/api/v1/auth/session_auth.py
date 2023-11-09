335b78fee62030678e33a6ccfb41ab6566b1c6d8
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-11-09 19:42:52 +0200

    Sessions authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+SessionAuth class defined here
+"""
+from flask import abort, request
+from api.v1.auth.auth import Auth
+from typing import Tuple, TypeVar
+import uuid
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a new session """
+        if not user_id:
+            return None
+        if type(user_id) != str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ retreives user_id of a session_id """
+        if session_id is None:
+            return None
+        if type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ get current user """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        user = User.get(user_id)
+        return user
+
+    def destroy_session(self, request=None):
+        """ deletes a session """
+        if request is None:
+            return False
+        sess_id = self.session_cookie(request)
+        if not sess_id:
+            return False
+        user_id = self.user_id_for_session_id(sess_id)
+        if not user_id:
+            return False
+        del self.user_id_by_session_id[sess_id]
+        return True
